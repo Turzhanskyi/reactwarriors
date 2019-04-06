5e9b5368ba1f7b8ed43d5e784f0ebe016ea47e2f
Turzhanskyi <turzhansky81@gmail.com>
2019-04-06 15:02:40 +0300

    Step 2. Первый компонент + props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css'
 
+function MovieItem(props) {
+  console.log("props = ", props);
+  return(
+      <div>
+        <p>{props.title}</p>
+        <p>{props.vote_average}</p>
+      </div>
+  )
+}
+
 function App() {
-  return<div className="div">Hello react Warriors!!!</div>
+  return(
+      <div className="div">
+        <MovieItem title="Title new" vote_average={10.1}/>
+      </div>
+      )
 }
 
 export default App;
